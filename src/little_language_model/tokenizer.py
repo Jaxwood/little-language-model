3b90a6d176ec4577daf1a291b7e6d592316409cd
Jacob Lorenzen <jacob@lorenzen.me>
2025-08-13 09:44:56 +0200

    feat: tokenize text

--- src/little_language_model/tokenizer.py
+++ src/little_language_model/tokenizer.py
@@ -1,7 +1,49 @@
-def tokenize(text):
-    """
-    Splits the input text into words using whitespace.
-    Returns a list of words.
-    """
-    return text.split()
+import tiktoken
+import torch
+from torch.utils.data import Dataset, DataLoader
 
+class GPTDataset(Dataset):
+    def __init__(self, txt, tokenizer, max_length=256, stride=128):
+        """
+        Initializes the dataset with a text and tokenizer.
+        Reads the file line by line and tokenizes each line.
+        """
+        self.input_ids = []
+        self.target_ids = []
+
+        token_ids = tokenizer.encode(txt)
+        for i in range(0, len(token_ids) - max_length, stride):
+            input_chunk = token_ids[i:i + max_length]
+            target_chunk = token_ids[i + 1:i + max_length + 1]
+            self.input_ids.append(torch.tensor(input_chunk))
+            self.target_ids.append(torch.tensor(target_chunk))
+
+    def __len__(self):
+        """
+        Returns the number of samples in the dataset.
+        """
+        return len(self.input_ids)
+
+    def __getitem__(self, idx):
+        """
+        Returns the input and target tensors for a given index.
+        """
+        return self.input_ids[idx], self.target_ids[idx]
+
+def data_loader(file_path, batch_size=4, max_length=256, stride=128, shuffle=True, drop_last=True, num_workers=0):
+    """
+    Loads data from a file and returns a DataLoader object.
+    """
+    with open(file_path, 'r') as txt:
+        file_contents = txt.read()
+        tokenizer = tiktoken.get_encoding("gpt2")
+        dataset = GPTDataset(file_contents, tokenizer, max_length, stride)
+        dataloader = DataLoader(
+            dataset,
+            batch_size=batch_size,
+            shuffle=shuffle,
+            drop_last=drop_last,
+            num_workers=num_workers
+        )
+        data_iter = iter(dataloader)
+        return next(data_iter, None)
--- tests/test_tokenizer.py
+++ tests/test_tokenizer.py
@@ -1,7 +1,6 @@
 import pytest
-from little_language_model.tokenizer import tokenize
+from little_language_model.tokenizer import data_loader
 
-def test_tokenize_simple_sentence():
-    sentence = "Hello world, this is a test."
-    expected = ["Hello", "world,", "this", "is", "a", "test."]
-    assert tokenize(sentence) == expected
+def test_data_loader():
+    result = data_loader("data/the-verdict.txt")
+    assert result != []
